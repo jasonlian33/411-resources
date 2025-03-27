160220b41a83ee019dedabac593931e1e69b804c
jason lian <jasonlian16@gmail.com>
2025-03-27 19:15:03 -0400

    Implemented Docker Setup

--- HW/HW2 Codebase/boxing/run_docker.sh
+++ HW/HW2 Codebase/boxing/run_docker.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Variables
-IMAGE_NAME=
-CONTAINER_TAG=
-HOST_PORT=5000
-CONTAINER_PORT=5000
-DB_VOLUME_PATH=  # Adjust this to the desired host path for the database persistence
-BUILD=  # Set this to true if you want to build the image
+IMAGE_NAME="boxing"
+CONTAINER_TAG="homework"
+HOST_PORT=5001
+CONTAINER_PORT=5001
+DB_VOLUME_PATH="./db" # Adjust this to the desired host path for the database persistence
+BUILD=true  # Set this to true if you want to build the image
 
 # Check if we need to build the Docker image
 if [ "$BUILD" = true ]; then
   echo "Building Docker image..."
-
+docker build -t ${IMAGE_NAME}:${CONTAINER_TAG} .
 else
   echo "Skipping Docker image build..."
 fi
@@ -19,17 +19,18 @@ fi
 # Check if the database directory exists; if not, create it
 if [ ! -d "${DB_VOLUME_PATH}" ]; then
   echo "Creating database directory at ${DB_VOLUME_PATH}..."
-
+mkdir -p ${DB_VOLUME_PATH}
 fi
 
 # Stop and remove the running container if it exists
 if [ "$(docker ps -q -a -f name=${IMAGE_NAME}_container)" ]; then
     echo "Stopping running container: ${IMAGE_NAME}_container"
-
+    docker stop ${IMAGE_NAME}_container
 
     # Check if the stop was successful
     if [ $? -eq 0 ]; then
         echo "Removing container: ${IMAGE_NAME}_container"
+        docker rm ${IMAGE_NAME}_container
 
     else
         echo "Failed to stop container: ${IMAGE_NAME}_container"
@@ -41,5 +42,9 @@ fi
 
 # Run the Docker container with the necessary ports and volume mappings
 echo "Running Docker container..."
-
-echo "Docker container is running on port ${HOST_PORT}."
+docker run -d \
+  --name ${IMAGE_NAME}_container \
+  -p ${HOST_PORT}:${CONTAINER_PORT} \
+  -v ${DB_VOLUME_PATH}:/path/inside/container \
+  ${IMAGE_NAME}:${CONTAINER_TAG}
+  echo "Docker container is running on port ${HOST_PORT}."
