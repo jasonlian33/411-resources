d7938375fcae498c9024fee34eb043eb4905279c
jason lian <jasonlian16@gmail.com>
2025-03-31 22:35:56 -0400

    Added ENV_FILE to Docker

--- HW/HW2 Codebase/boxing/run_docker.sh
+++ HW/HW2 Codebase/boxing/run_docker.sh
@@ -45,6 +45,6 @@ echo "Running Docker container..."
 docker run -d \
   --name ${IMAGE_NAME}_container \
   -p ${HOST_PORT}:${CONTAINER_PORT} \
-  -v ${DB_VOLUME_PATH}:/app/data \
+  -v ${DB_VOLUME_PATH}:/app/db \
   ${IMAGE_NAME}:${CONTAINER_TAG}
   echo "Docker container is running on port ${HOST_PORT}."
