ca56901766d70a13eeb8d49904168963c07d8bf3
BuildEternal <94197506+BuildEternal@users.noreply.github.com>
2025-04-17 19:30:03 -0400

    Replace deprecated usage in mixins.py

--- HW/HW3 caching/boxing/boxing/models/mixins.py
+++ HW/HW3 caching/boxing/boxing/models/mixins.py
@@ -1,10 +1,10 @@
-from datetime import datetime
+from datetime import UTC, datetime
 from sqlalchemy import Column, DateTime
 
 
 class TimestamperMixin:
-    created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
 
     def pretty_timestamp(self):
         return self.created_at.strftime("%Y-%m-%d %H:%M:%S")
@@ -19,10 +19,10 @@ class ReprMixin:
 
 class LoggerMixin:
     def log_create(self):
-        print(f"[CREATE] {self.__class__.__name__} created at {datetime.utcnow()}")
+        print(f"[CREATE] {self.__class__.__name__} created at {datetime.now(UTC)}")
 
     def log_update(self):
-        print(f"[UPDATE] {self.__class__.__name__} updated at {datetime.utcnow()}")
+        print(f"[UPDATE] {self.__class__.__name__} updated at {datetime.now(UTC)}")
 
     def log_delete(self):
-        print(f"[DELETE] {self.__class__.__name__} deleted at {datetime.utcnow()}")
+        print(f"[DELETE] {self.__class__.__name__} deleted at {datetime.now(UTC)}")
